daf5cab309f00d8c1ebaac272ccd84da9af7ba58
Aidan <psyaoc@gmail.com>
2023-07-20 20:41:59 +0100

    test hellos with multiple names

--- greetings/greetings_test.go
+++ greetings/greetings_test.go
@@ -20,3 +20,22 @@ func TestHelloEmpty(t *testing.T) {
 		t.Fatalf(`Hello("") = %q, %v, want "", error`, message, error)
 	}
 }
+
+func TestHellosName(t *testing.T) {
+	names := []string{"Bob", "Tim", "John"}
+
+	messages, error := Hellos(names)
+
+	if len(messages) != len(names) || error != nil {
+		t.Fatalf("hellos produced an unexpected error, or length of messages")
+	}
+
+	for _, name := range names {
+		want := regexp.MustCompile(`\b` + name + `\b`)
+		message := messages[name]
+		if !want.MatchString(message) {
+			t.Fatalf(`Hellos map, key: %v = %q, %v, want match for %#q, nil`, name, message, error, want)
+		}
+	}
+
+}
