b9be878b744d5ef98e30bee16d5e833d87053951
Aidan <psyaoc@gmail.com>
2023-07-20 20:14:04 +0100

    return an error if no name is provided

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,16 +1,23 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 /* func declared starting with a capital can be called by a func
 not in the same package */
 // known as exported name
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+	// return an error if no name is provided
+	if name == "" {
+		return "", errors.New("no name")
+	}
 	// declare and initialise
 	/*
 		var message string
 		message = fmt.Sprintf("Hi, %v. Welcome!", name)
 	*/
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message
+	return message, nil
 }
